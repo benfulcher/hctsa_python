c56a32a4ee1ef8e9b4bdd3ab31ca1a5f0aa54f55
Ben Fulcher <ben.d.fulcher@gmail.com>
2016-02-03 11:38:12 +1100

    added link to open hctsa package

--- tsStats.py
+++ tsStats.py
@@ -7,10 +7,10 @@ def evaluateAllFunctions(y,functionDic):
     """
     Evalutes all functions specified in a list.
     """
-    
+
     return {label: function(y) for label, function in functionDic.iteritems()}
-    
-    
+
+
     # dic = {}
     # for label in functionDic:
     #     # print label
@@ -26,10 +26,10 @@ def convertToFeatureVector(outputDic):
     Converts an output dictionary to two lists: each feature has a name
     and a corresponding output.
     """
-    
+
     nameList = []
     outputList = []
-    
+
     for label in outputDic:
         if type(outputDic[label]) is dict:
             for subLabel in outputDic[label]:
@@ -38,20 +38,20 @@ def convertToFeatureVector(outputDic):
         else:
             nameList.append(label)
             outputList.append(outputDic[label])
-            
+
     return nameList, outputList
 
 def vectorize(time_series):
-    
+
     """
     Converts an input time-series object to a numpy ndarray vector
-    
+
     Arguments
     ---------
-    
+
     time_series: some time-series format
     """
-    
+
     if type(time_series) is ts.TimeSeries:
         return time_series.data
     elif type(time_series) is np.ndarray:
@@ -63,13 +63,13 @@ def vectorize(time_series):
 def makeRowVector(time_series):
     """
     Ensures a vector is a row, as required by many numpy functions (like diff)
-    
+
     Arguments
     ---------
-    
+
     time_series: a np.ndarray (use vectorize)
     """
-    
+
     if len(time_series.shape) == 1:
         # For some reason if you only pass a single argument it does this shit:
         time_series.shape = (1,len(time_series))
@@ -86,27 +86,27 @@ def ST_Length(y):
     """
     Returns the time-series length
     """
-    
+
     # Make the input a row vector of numbers:
     y = makeRowVector(vectorize(y))
-    
+
     # How long is this row vector?:
     return y.shape[1]
 
 def DN_Means(y,meanType='arithmetic'):
-    
+
     # Make the input a row vector of numbers:
     y = makeRowVector(vectorize(y))
-    
+
     # Return mean:
     if meanType in set(['arithmetic','norm']):
         return np.mean(y);
 
 def DN_Spread(y,stdType='std'):
-    
+
     # Make the input a row vector of numbers:
     y = makeRowVector(vectorize(y))
-    
+
     # Return standard deviation of time-series values:
     if stdType == 'std':
         return np.std(y);
@@ -116,7 +116,7 @@ def EN_CID(y):
     CID measure from Batista, G. E. A. P. A., Keogh, E. J., Tataw, O. M. & de
     Souza, V. M. A. CID: an efficient complexity-invariant distance for time
     series. Data Min Knowl. Disc. 28, 634-669 (2014).
-    
+
     Arguments
     ---------
 
@@ -129,7 +129,7 @@ def EN_CID(y):
 
     # Prepare the output dictionary
     out = {}
-    
+
      # Original definition (in Table 2 of paper cited above)
     out['CE1'] = np.sqrt(np.mean(np.power(np.diff(y),2))); # sum -> mean to deal with non-equal time-series lengths
 
@@ -143,14 +143,14 @@ def SB_MotifTwo(y,binarizeHow='diff'):
     """
     Looks at local motifs in a binary symbolization of the time series, which is performed by a
     given binarization method
-    
+
     Arguments
     ---------
 
     y: a nitime time-series object, or numpy vector
 
     """
-    
+
     # Make the input a row vector of numbers:
     y = makeRowVector(vectorize(y))
 
@@ -159,31 +159,32 @@ def SB_MotifTwo(y,binarizeHow='diff'):
         yBin = ((np.sign(np.diff(y)))+1.)/2.
     else:
         raise ValueError(binarizeHow)
-        
+
     # Initialize output dictionary
     out = {}
-    
+
     # Where the difference is 0, 1
     r0 = yBin==0
     r1 = yBin==1
-    
+
 
     out['u'] = np.mean(r1)
     out['d'] = np.mean(r0)
     out['h'] = -(out['u']*np.log2(out['u']) + out['d']*np.log2(out['d']))
-    
+
     return out
 
 def DN_Burstiness(y):
     """
     Returns the 'burstiness' statistic from:
-    Goh and Barabasi, 'Burstiness and memory in complex systems' Europhys. Lett.
-    % 81, 48002 (2008)
+    Goh and Barabasi,
+    'Burstiness and memory in complex systems',
+    Europhys. Lett. 81, 48002 (2008)
     """
-    
+
     # Ensure the input is a row vector of numbers:
     y = makeRowVector(vectorize(y))
-    
+
     # Compute the burstiness statistic, B:
     B = (np.std(y) - np.mean(y))/(np.std(y) + np.mean(y))
     return B
@@ -214,4 +215,3 @@ functionNames, outputs = convertToFeatureVector(resultsDic)
 
 for i in np.arange(len(functionNames)):
     print functionNames[i],' = ',outputs[i]
-
